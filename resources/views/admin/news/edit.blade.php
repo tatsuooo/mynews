2a2038446745990d2299414510d4c607233edc3c
EC2 Default User <ec2-user@ip-172-31-43-72.us-east-2.compute.internal>
2021-05-16 15:21:14 +0000

    bladeの課題

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -8,6 +8,6 @@
         <title>MyNews</title>
     </head>
     <body>
-        <h1>Myニュース作成画面</h1>
+        <h1>課題作成画面</h1>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,11 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+//課題4
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('profile/create', 'Admin\ProfileController@add');
+    Route::get('profile/edit','Admin\ProfileController@edit');
+    
+    
 });
