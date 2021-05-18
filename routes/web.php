16570557d07ca0c8ed96d804488e5c18435a9c6e
EC2 Default User <ec2-user@ip-172-31-43-72.us-east-2.compute.internal>
2021-05-18 15:59:42 +0000

    profile

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit','Admin\ProfileController@edit');
-    
+    Route::get('news/create', 'Admin\NewsController@add');
     
 });
