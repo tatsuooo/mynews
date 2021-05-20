6f171bed74a9dbc14f61670d331cb72e5bbcbbd4
EC2 Default User <ec2-user@ip-172-31-43-72.us-east-2.compute.internal>
2021-05-20 17:26:12 +0000

    課題13の6

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::get('/', function () {
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::get('profile/create', 'Admin\profileController@add');
      Route::post('profile/create', 'Admin\profileController@create'); # 追記
-});
+});  Route::post('profile/edit', 'Admin\profileController@update');
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
